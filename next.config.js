0cf886f095d7bf31bcb8eb1bb93814b1e997d7bd
kevelscy <kevelscy@gmail.com>
2022-11-24 22:06:28 -0400

    update: rewrite back-api url

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
         {
           source: '/api/:path*',
-          destination: process.env.NODE_ENV !== 'production' ? `${process.env.API_URL}/:path*` : `http://localhost:8000/:path*`
+          destination: process.env.NODE_ENV !== 'production' ? `${process.env.API_URL}/api/:path*` : `http://localhost:8000/api/:path*`
         },
         {
           source: '/',
