c0d13fc48a1dad88ce117657c306025da0a89293
kevelscy <kevinblanco@turpialdev.com>
2023-02-07 06:31:06 -0400

    lint + use API_URL env

--- next.config.js
+++ next.config.js
@@ -7,39 +7,39 @@ module.exports = {
   },
   async rewrites () {
     return [
-        {
-          source: '/api/:path*',
-          // destination: `${process.env.API_URL}/api/:path*`
-          destination: `http://localhost:8000/api/:path*`
-        },
-        {
-          source: '/',
-          destination: '/dashboard'
-        },
-        {
-          source: '/tienda/:path*',
-          destination: '/proximamente',
-        },
-        {
-          source: '/multimedia',
-          destination: '/proximamente',
-        },
-        {
-          source: '/usuarios',
-          destination: '/proximamente',
-        },
-        {
-          source: '/eventos',
-          destination: '/proximamente',
-        },
-        {
-          source: '/devocionales',
-          destination: '/proximamente',
-        },
-        {
-          source: '/articulos',
-          destination: '/proximamente',
-        },
-      ]
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`
+        // destination: `http://localhost:8000/api/:path*`
+      },
+      {
+        source: '/',
+        destination: '/dashboard'
+      },
+      {
+        source: '/tienda/:path*',
+        destination: '/proximamente'
+      },
+      {
+        source: '/multimedia',
+        destination: '/proximamente'
+      },
+      {
+        source: '/usuarios',
+        destination: '/proximamente'
+      },
+      {
+        source: '/eventos',
+        destination: '/proximamente'
+      },
+      {
+        source: '/devocionales',
+        destination: '/proximamente'
+      },
+      {
+        source: '/articulos',
+        destination: '/proximamente'
+      }
+    ]
   }
 }
